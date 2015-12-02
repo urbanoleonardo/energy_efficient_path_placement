a80efd3ceae7d7694956e5c1839188bd65b0fe5d
Enrico <Enrico@Enrico-PC>
2015-12-02 12:03:47 +0100

    Robot: changes to multiply matrices Robot_constructor: more tests on inverse matrices and such

--- src/Robot.java
+++ src/Robot.java
@@ -295,20 +295,24 @@ public class Robot {
 		 * Function implemented to multiply 2 matrices (useful in Hfrom_to)
 		 */
 
-		double[][] m = new double[left.length][right[0].length];
+		double[][] M = new double[left.length][right[0].length];
 
+		for(int row = 0; row < M.length; row++)
+			for(int column = 0; column < M[0].length; column++)
+				M[row][column] = 0;
+		
 		if(left[0].length == right.length){
 
-			for(int row = 0; row < m.length; row++)
-				for(int column = 0; column < m[0].length; column++)
+			for(int row = 0; row < M.length; row++)
+				for(int column = 0; column < M[0].length; column++)
 					for(int i = 0; i < left[0].length; i++)
-					m[row][i] += left[row][i] + right[i][column];
+					M[row][column] += left[row][i] * right[i][column];
 
 		}else{
 			System.out.println("The dimension of the matrices do not agree. [m*n n*p = m*p]");
 		}
 
-		return m;
+		return M;
 
 	}
 
--- src/Test_robot_constructor.java
+++ src/Test_robot_constructor.java
@@ -72,15 +72,79 @@ public class Test_robot_constructor {
 		Trajectory test_output = test_path.GetTrajectory();
 		System.out.println("Numero di punti : " + test_output.Points.size());
 		
-		/*
+		
 		for(Target i : test_output.Points)
 		{
 			double[] position_vector = i.getPosition();
 			
 			System.out.println("Punto : " + position_vector[0] + " " + position_vector[1] + " " + position_vector[2]);
 		}
+		
+		//Testing Target methods
+		
+		double[][] matrix3 = {
+				{1.0, Math.PI, 0.0, 4.6},
+				{2.1, 1.0, 0.0, 4.0},
+				{2.45, 0.0, 1.0, -1.0},
+				{0.0, 0.0, 0.0, 1.0}
+		};
+		Target test_target3 = new Target(matrix3);
+		double[][] test = test_target3.getInvHomMatrix();
+		/*
+		double[][] test = {
+				{1.0, 0.0, 0.0, 0},
+				{0, 1.0, 0.0, 0},
+				{0, 0.0, 1.0, 0},
+				{0.0, 0.0, 0.0, 1}
+		};
 		*/
 		
+		double[][] test_ident = MultiplyMatrices(test,matrix3);
+		
+		System.out.println("Righe : " + test_ident.length + " Colonne : " + test_ident[0].length);
+		
+		for(int i=0; i< test_ident.length;i++)
+		{
+			for(int j=0; j<test_ident[0].length; j++)
+			{
+				System.out.print(test_ident[i][j] + " ");
+			}
+			System.out.println(" ");
+		}
+		
 	}
+	
+	public static double[][] MultiplyMatrices(double[][] left, double[][] right){
 
+		/*
+		 * Function implemented to multiply 2 matrices (useful in Hfrom_to)
+		 */
+
+		double[][] M = new double[left.length][right[0].length];
+		
+		for(int row = 0; row < M.length; row++)
+			for(int column = 0; column < M[0].length; column++)
+				M[row][column] = 0;
+		
+
+		if(left[0].length == right.length){
+
+			for(int row = 0; row < M.length; row++)
+			{
+				for(int column = 0; column < M[0].length; column++)
+				{
+					for(int i = 0; i < left[0].length; i++)
+					{
+					M[row][column] += left[row][i] * right[i][column];
+				}
+				}
+		
+			}
+
+		}else{
+			System.out.println("The dimension of the matrices do not agree. [m*n n*p = m*p]");
+		}
+
+		return M;
+}
 }
