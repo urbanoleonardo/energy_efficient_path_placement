6a543dc91b6c71bf6c407046c695602f5d1475ea
Leonardo Urbano <urbanoleonardo91@gmail.com>
2015-12-02 10:40:05 +0100

    Bugs fixed.

--- src/Target.java
+++ src/Target.java
@@ -10,11 +10,30 @@ public class Target {
 	public static void main(String[] args){
 
 	}
-	
-	
-	
-	
+
 	//Part of CONSTRUCTORS
+
+	public Target(){
+
+		hom_matrix = new double[4][4];
+		position = new double[3];
+		rotation = new double[3][3];
+
+		double[][] I = new double[4][4];
+		I = identityMatr();
+
+		for(int i = 0; i < I.length; i++)
+			for (int j = 0; j < I[i].length; j++){
+				hom_matrix[i][j] = I[i][j];
+				if(i != 3 && j == 3)
+					position[i] = I[i][j];
+				if(i != 3 && j != 3)
+					rotation[i][j] = I[i][j];
+			}
+
+		return;
+
+	}
 
 	public Target(double[][] T){
 
@@ -22,13 +41,13 @@ public class Target {
 		position = new double[3];
 		rotation = new double[3][3];
 
-		if(T.length == 4 || T[0].length == 4){		
+		if(T.length == 4 && T[0].length == 4){		
 			for(int i = 0; i < T.length; i++)
 				for (int j = 0; j < T[i].length; j++){
 					hom_matrix[i][j] = T[i][j];
-					if(i != 4 && j == 4)
+					if(i != 3 && j == 3)
 						position[i] = T[i][j];
-					if(i != 4 && j != 4)
+					if(i != 3 && j != 3)
 						rotation[i][j] = T[i][j];
 				}
 		}else{
@@ -44,10 +63,10 @@ public class Target {
 		position = new double[3];
 		rotation = new double[3][3];
 
-		hom_matrix[3][3] = 1;
+		hom_matrix = identityMatr();
 
-		if(rotation.length == 3){
-			for(int i = 0; i < pos.length;i++){
+		if(pos.length == 3){
+			for(int i = 0; i < pos.length; i++){
 				hom_matrix[i][3] = pos[i];
 				position[i] = pos[i];
 			}
@@ -56,9 +75,9 @@ public class Target {
 			return;
 		}
 
-		if(rot.length == 3 || rot[0].length == 3){
-			for(int i = 0; i < 3; i++)
-				for(int j = 0; j < 3; j++){
+		if(rot.length == 3 && rot[0].length == 3){
+			for(int i = 0; i < rot.length; i++)
+				for(int j = 0; j < rot[0].length; j++){
 					hom_matrix[i][j] = rot[i][j];
 					rotation[i][j] = rot[i][j];
 				}
@@ -69,10 +88,10 @@ public class Target {
 	}
 
 	//END of CONSTRUCTORS
-	
-	
+
+
 	//Part of GET methods
-	
+
 	public double[][] getHomMatrix(){
 
 		return hom_matrix;
@@ -90,22 +109,87 @@ public class Target {
 		return rotation;
 
 	}
-	
+
 	//END of GET methods
-	
+
 	//OTHER PUBLIC methods
-	
-	
-	
-	
-	
+
+	public Target inv(){
+
+		double[][] INV = new double[4][4];
+
+		INV = gaussianElimination(this.getHomMatrix());
+
+		Target T = new Target(INV);
+
+		return T;
+
+
+
+	}
+
 	//END of PUBLIC methods
-	
-	
-	
+
+
+
 	//PRIVATE methods
-	
-	
-	
+
+	private double[][] identityMatr(){
+
+		double[][] I = new double[4][4];
+
+		for(int i = 0; i < I.length; i++)
+			for(int j = 0; j < I[0].length; j++)
+				if(i == j)
+					I[i][j] = 1;
+				else I[i][j] = 0;
+
+		return I;
+
+	}
+
+	private double[][] gaussianElimination(double[][] M){
+		// TODO: test this!!
+		/*
+		 * It returns the inverse matrix using Gaussian Elimination Algorithm
+		 * Input: 4x4 matrix
+		 */
+
+		double temp;
+		double[][] INV = new double[4][4];
+		INV = identityMatr();
+
+		for(int k = 0; k < M.length; k++){
+
+			temp = M[k][k];	
+
+			for(int j = 0; j < M[0].length; j++){
+
+				M[k][j] /= temp;									
+				INV[k][j] /= temp;		
+
+			}
+
+			for(int i=0; i < M.length; i++){
+
+				temp = M[i][k];	
+
+				for(int j = 0; j < M[0].length; j++){
+
+					if(i == k)
+						break;
+
+					M[i][j] -= M[k][j]*temp;						
+					INV[i][j] -= INV[k][j]*temp;
+
+				}
+			}
+		}
+
+		return INV;
+
+	}
+
+	//END of PRIVATE methods
 
 }
